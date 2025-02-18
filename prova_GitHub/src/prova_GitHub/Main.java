653457254fcabdf11a5989f014c275199b3203d3
nikita <nmatvei@insgabrielamistral.cat>
2025-02-18 12:00:07 +0100

    2a modificació

--- prova_GitHub/src/prova_GitHub/Main.java
+++ prova_GitHub/src/prova_GitHub/Main.java
@@ -3,5 +3,7 @@ package prova_GitHub;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("1a modificació Git");
+		
+		System.out.println("2a modificació GitHub");
 	}
 }
